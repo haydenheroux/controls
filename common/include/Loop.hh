302126c19f8c5dfcd29dd899e91dfcae4ca98454
Hayden Heroux <haydenheroux@gmail.com>
2026-02-12 15:12:40 -0500

    feat(common): Add loop overrun detection.

--- common/include/Loop.hh
+++ common/include/Loop.hh
@@ -2,6 +2,7 @@
 
 #include <chrono>
 #include <thread>
+#include "units.hh"
 
 namespace reefscape {
 
@@ -9,17 +10,23 @@ class Loop {
   quantities::Time time_step_;
   std::chrono::microseconds time_step_microseconds_;
   quantities::Time total_time_;
+  quantities::Time last_tick_runtime_;
 
 public:
   Loop(quantities::Time time_step)
       : time_step_(time_step),
         time_step_microseconds_(std::chrono::microseconds(
             time_step.in<int>(au::micro(au::seconds)))),
-        total_time_(au::seconds(0)) {}
+        total_time_(au::seconds(0)),
+        last_tick_runtime_(au::seconds(0)) {}
 
   template <typename F>
   void Tick(F &&tick) {
+    auto start = std::chrono::steady_clock::now();
     tick();
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::microseconds delta = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    last_tick_runtime_ = (au::micro(au::seconds))(delta.count());
     total_time_ += time_step_;
     std::this_thread::sleep_for(time_step_microseconds_);
   }
@@ -32,6 +39,10 @@ public:
   }
 
   quantities::Time TotalTime() const { return total_time_; }
+
+  quantities::Time LastTickTime() const { return last_tick_runtime_; }
+
+  bool Overran() const { return last_tick_runtime_ > time_step_; }
 };
 
 }  // namespace reefscape
